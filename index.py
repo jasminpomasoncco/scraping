42eea9da6d33f7cf18aef91eb706fdc2653b5881
jasminpomasoncco <u20201b236@upc.edu.pe>
2024-07-02 00:25:04 -0500

    update using Beautiful Soup

--- index.py
+++ index.py
@@ -3,12 +3,54 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-website = "https://www.vulnhub.com"
+def scrape_website(url):
+    result = requests.get(url)
+    content = result.text
+    soup = BeautifulSoup(content, 'html.parser')
 
-# example: <a href="/entry/web-machine-n7,756/">Web Machine: (N7)</a>
-result = requests.get(website)
-content = result.text
-soup = BeautifulSoup(content, 'html.parser')
+    data_all = []
+    unique_data = {
+        'Machine Name': set(),
+        'Machine Date': set(),
+        'Author': set()
+    }
+
+    links = soup.find_all('a', href=True)
+
+    for link in links:
+        href = link['href']
+        link_text = link.get_text(strip=True)
+        
+
+        if "/entry/" in href:
+            machine_date = ""
+            machine_name = ""
+            prev_element1 = link.find_parent('div', class_='card-date')
+            prev_element2 = link.find_parent('div', class_='card-title')
+
+            if prev_element1:
+                machine_date = prev_element1.get_text(strip=True)
+            if prev_element2:
+                machine_name = prev_element2.get_text(strip=True)
+
+            
+            data_all.append({
+                'Machine Name': machine_name,
+                'Machine Date': machine_date,
+                'Author': "" 
+            })
+            unique_data['Machine Name'].add(machine_name)
+            unique_data['Machine Date'].add(machine_date)
+
+        elif "/author/" in href:
+            machine_author = link_text.strip()
+
+            if data_all:
+                data_all[-1]['Author'] = machine_author
+
+            unique_data['Author'].add(machine_author)
+
+    return data_all, unique_data
 
 # Define patterns and associated lists
 # patterns = {
@@ -22,30 +64,20 @@ soup = BeautifulSoup(content, 'html.parser')
 #     }
 # }
 # Define patterns and associated lists
-patterns = {
-    'machine_name': {
-        'selector': 'div.card-title',  # Selector CSS
-        'list': []
-    },
-    'machine_date': {
-        'selector': 'div.card-date',  # Selector CSS
-        'list': []
-    },
-    'author': {
-        'selector': 'div.card-author',  # Selector CSS
-        'list': []
-    }
-}
-
-# links = soup.find_all('a', href=True)
-
-# machine_names = []
-# machine_dates = []
-# machine_authors = []
-
-# for link in links:
-#     href = link['href']
-#     link_text = link.get_text(strip=True)
+# patterns = {
+#     'machine_name': {
+#         'selector': 'div.card-title',  # Selector CSS
+#         'list': []
+#     },
+#     'machine_date': {
+#         'selector': 'div.card-date',  # Selector CSS
+#         'list': []
+#     },
+#     'author': {
+#         'selector': 'div.card-author',  # Selector CSS
+#         'list': []
+#     }
+# }
 
 #     for pattern, storage in patterns.items():
 #         if pattern.match(href):
@@ -64,26 +96,26 @@ patterns = {
 #     if 'list' in storage:
 #             storage['list'] = list(set(storage['list']))
 
-for key, info in patterns.items():
-    elements = soup.select(info['selector'])
-    for element in elements:
-        if key == 'machine_name':
-            link = element.find('a', href=True)
-            if link:
-                info['list'].append(link.get_text(strip=True))
-        elif key == 'machine_date':
-            info['list'].append(element.get_text(strip=True))
-        elif key == 'author':
-            info['list'].append(element.get_text(strip=True))
+# for key, info in patterns.items():
+#     elements = soup.select(info['selector'])
+#     for element in elements:
+#         if key == 'machine_name':
+#             link = element.find('a', href=True)
+#             if link:
+#                 info['list'].append(link.get_text(strip=True))
+#         elif key == 'machine_date':
+#             info['list'].append(element.get_text(strip=True))
+#         elif key == 'author':
+#             info['list'].append(element.get_text(strip=True))
 
-for info in patterns.values():
-    info['list'] = list(set(info['list']))
+# for info in patterns.values():
+#     info['list'] = list(set(info['list']))
 
 #final_machine_names = [name.replace("/entry/", "") for name in patterns[re.compile(r"/entry/[\w-]*")]['name_list']]
 #last_machine = "noob-1"
 #exist_last_machine = last_machine in final_machine_names
 
-print("Máquinas únicas:", patterns['machine_name'])
+#print("Máquinas únicas:", patterns['machine_name'])
 #print("Fechas únicas:", patterns[re.compile(r"/entry/[\w-]*")]['date_list'])
 #print("Autores únicos:", patterns[re.compile(r"/author/[\w-]*")]['list'])
 
@@ -93,14 +125,31 @@ print("Máquinas únicas:", patterns['machine_name'])
 #     print("***There are new machines***")
 
 # Export in CSV
-with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['Machine Name', 'Machine Date', 'Author'])
+def export_to_csv(data, filename):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Machine Name', 'Machine Date', 'Author']
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        csvwriter.writeheader()
 
-    max_length = max(len(patterns['machine_name']['list']), len(patterns['machine_date']['list']), len(patterns['author']['list']))
+        for entry in data:
+            csvwriter.writerow({
+                'Machine Name': entry['Machine Name'],
+                'Machine Date': entry['Machine Date'],
+                'Author': entry['Author']
+            })
 
-    for i in range(max_length):
-        name = patterns['machine_name']['list'][i] if i < len(patterns['machine_name']['list']) else ''
-        date = patterns['machine_date']['list'][i] if i < len(patterns['machine_date']['list']) else ''
-        author = patterns['author']['list'][i] if i < len(patterns['author']['list']) else ''
-        csvwriter.writerow([name, date, author])
+def export_unique_to_csv(unique_data, filename_prefix):
+    for key, values in unique_data.items():
+        filename = f'{filename_prefix}_{key.lower().replace(" ", "_")}_unique.csv'
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([key])
+            csvwriter.writerows([[value] for value in values])
+
+if __name__ == "__main__":
+    website = "https://www.vulnhub.com"
+    data_all, unique_data = scrape_website(website)
+
+    export_to_csv(data_all, 'data_all.csv')
+    export_unique_to_csv(unique_data, 'unique_values')
+    print(data_all)
